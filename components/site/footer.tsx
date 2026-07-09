b227282585f996aba44de120f97950898d2ad14a
DarulArqumTest <yolobolo990@gmail.com>
2026-07-09 00:58:33 -0400

    Create missing Footer component

---
+++ components/site/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white">
+      <div className="container mx-auto px-4 py-8">
+        <p>&copy; 2024 DarulArqum. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
