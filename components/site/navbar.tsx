e246963cc53c8f13f75aedef30591d07e4643f54
DarulArqumTest <yolobolo990@gmail.com>
2026-07-09 00:58:29 -0400

    Create missing Navbar component

---
+++ components/site/navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+  return (
+    <nav>
+      <div className="container mx-auto px-4 py-4">
+        <h1 className="text-xl font-bold">DarulArqum</h1>
+      </div>
+    </nav>
+  );
+}
